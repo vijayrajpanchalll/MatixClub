a9085317e2fd93620d2a56abc592e9bcc8b60707
vijayrajpanchalll <vijay@singhsoft.com>
2023-11-20 11:32:36 +0530

    add deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,40 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const EverGreen = await ethers.getContractFactory("EverGreen");
+  //get account from hardhat
+  const accounts = await ethers.getSigners();
+  console.log("Deploying EverGreen with the account:", accounts[0].address);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const EverGreenContract = await EverGreen.deploy(accounts[0].address);
+  await EverGreenContract.deployed();
+  
+  console.log("EverGreen deployed to:", EverGreenContract.address);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
 
-  await lock.waitForDeployment();
+  console.log("Waiting for 10 seconds before verifying contract");
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await new Promise(r => setTimeout(r, 10000));
+
+  const chainId = await ethers.provider.getNetwork();
+  if (chainId.chainId == 31337) {
+    console.log("Select a network other than Hardhat Network to deploy and verify contracts");
+  } else {
+    try {
+      await run(`verify:verify`, {
+        address: EverGreenContract.address,
+        constructorArguments: [accounts[0].address],
+      });
+    } catch (e) {
+      if (e.message.toLowerCase().includes("already verified")) {
+        console.log("Already verified!");
+      } else {
+        console.log(e);
+      }
+    }
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
